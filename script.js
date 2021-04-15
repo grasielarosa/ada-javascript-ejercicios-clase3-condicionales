5ac311e254a62cc1bf836bbb866dd849924139b6
grasielarosa <grasielarosaa@gmail.com>
2021-04-15 11:16:05 +0200

    ejercicios 1 a 5

---
+++ script.js
@@ -0,0 +1,23 @@
+// //####EJERCICIOS CONDICIONALES#####
+// 1 Puede ver película
+// const puedeVerPelicula = (edad, tieneAutorizacion) => {
+//     return (edad >= 15 || tieneAutorizacion);
+// }
+// EJERCICIO 2: Está en rango
+// const estaEnRango = (valor, minimo, maximo) => {
+//     return (valor <= maximo && valor >= minimo);
+// }
+// EJERCICIO 3: Puede avanzar
+// const puedeAvanzar = (color) => {
+//     return (color === 'verde');
+// }
+// EJERCICIO 4: ¿Es vocal ?
+//     let esVocal = (letra) => {
+//         return (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u');
+//     }
+
+// EJERCICIO 5: ¿Es consonante ?
+//     const esConsonante = (letra) => {
+//         let vocales = esVocal(letra);
+//         return !vocales
+//     }
